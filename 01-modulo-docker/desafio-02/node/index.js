58196ddea3180a4b39edf99a14e8e38136495b15
Felipe Amaral <amaral.felipe@live.com>
2021-09-13 09:17:28 -0300

    Concluido proxy reverso na app Node

--- 01-modulo-docker/desafio-02/node/index.js
+++ 01-modulo-docker/desafio-02/node/index.js
@@ -1,32 +1,41 @@
 const express = require('express')
+const mysql = require('mysql')
+const random_name =  require('node-random-name');
+
 const app = express()
 const port = 3000
+
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
     database:'nodedb'
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
+const connection = mysql.createConnection(config)
 const sqlCreate = `CREATE TABLE IF NOT EXISTS people (id INT NOT NULL auto_increment, name VARCHAR(250), PRIMARY KEY (id))`
 connection.query(sqlCreate, function (err, result) {
     if (err) throw err;
     console.log("Table created");
 });
+connection.end();
 
-const sqlInsert = `INSERT INTO people(name) values('Felipe Amaral')`
-connection.query(sqlInsert, function (err, result) {
-    if (err) throw err;
-    console.log("Data entered");
-});
+app.get('/', async(req, res) => {
+    var names = [];
+    const connection = mysql.createConnection(config);
 
-connection.end()
+    const sqlInsert = `INSERT INTO people(name) values('${random_name()}')`
+    connection.query(sqlInsert, function (err, result) {
+        if (err) throw err;
+        console.log("Data entered");
+    });
+    
+    connection.query('SELECT * FROM `people`', (error, results, fields)=> {
+        results.map(name => names.push(name.name))
+        res.send('<h1>Full Cycle Rocks!</h1><ul><li>'+names.join('</li><li>')+'</li></ul>')
+    });
 
-
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle Rocks!</h1>')
+    connection.end();
 })
 
 app.listen(port, ()=> {
